8ded9b24e426fe41e81493e8ae4b753a05a181bd
nik <volsknik@gmail.com>
2020-09-30 12:22:09 +0300

    ADD search_input locator

---
+++ src/main/java/locators/HomeLocator.java
@@ -0,0 +1,22 @@
+package locators;
+
+import org.openqa.selenium.By;
+
+public enum HomeLocator implements Locator {
+    YOUTUBE_LOGO(By.cssSelector("#logo[href='/']")),
+    SEARCH_INPUT(By.cssSelector("input#search")),
+
+
+
+    ;
+    private By path;
+
+    HomeLocator(By path) {
+        this.path = path;
+    }
+
+    @Override
+    public By getPath() {
+        return path;
+    }
+}
