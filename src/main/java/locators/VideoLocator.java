b8be6bf343ea19d7d19a09843fff550f68efa1be
nik <volsknik@gmail.com>
2020-09-30 13:21:21 +0300

    add next_button locator

--- src/main/java/locators/VideoLocator.java
+++ src/main/java/locators/VideoLocator.java
@@ -3,7 +3,11 @@ package locators;
 import org.openqa.selenium.By;
 
 public enum VideoLocator implements Locator {
+    NEXT_BUTTON(By.cssSelector(".ytp-next-button")),
+
+
     ;
+
     private By path;
 
     VideoLocator(By path) {
