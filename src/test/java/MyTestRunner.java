b12e0b62bb531006051329ccb94377abbc4bb351
nik <volsknik@gmail.com>
2020-09-30 11:29:44 +0300

    CREATE test/java/MyTestRunner class

---
+++ src/test/java/MyTestRunner.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public abstract class MyTestRunner {
+    WebDriver driver;
+
+    @BeforeClass
+    public void settingDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void destroyDriver() {
+        driver.quit();
+    }
+}
