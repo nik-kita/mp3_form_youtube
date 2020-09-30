d7d07741d2d2a473cf7c023672f36cf5833a454b
nik <volsknik@gmail.com>
2020-09-30 11:25:58 +0300

    CREATE BasePageObject

---
+++ src/main/java/pageobjects/BasePageObject.java
@@ -0,0 +1,11 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePageObject {
+    protected WebDriver driver;
+
+    public BasePageObject(WebDriver driver) {
+        this.driver = driver;
+    }
+}
